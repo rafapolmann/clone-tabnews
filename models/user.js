4b6332cfd9ddb2495806b93cac316756f4e6a9fd
Rafael Polmann <rafapolmann@hotmail.com>
2025-08-05 21:51:53 +0000

    feat: implement `/api/v1/user` endpoint with session renewal

--- models/user.js
+++ models/user.js
@@ -2,6 +2,37 @@ import database from "infra/database";
 import password from "models/password.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 
+async function findOneById(id) {
+  const userFound = await runSelectQuery(id);
+
+  return userFound;
+
+  async function runSelectQuery(id) {
+    const results = await database.query({
+      text: `
+    SELECT 
+      * 
+    FROM
+      users
+    WHERE
+      id = $1
+    LIMIT 
+      1
+    ;`,
+      values: [id],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O id informado não foi encontrado no sistema.",
+        action: "Verifique se o id está digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
 async function findOneByUsername(username) {
   const userFound = await runSelectQuery(username);
 
@@ -55,7 +86,7 @@ async function findOneByEmail(email) {
 
     if (results.rowCount === 0) {
       throw new NotFoundError({
-        message: "O email informado não foi encontrado no sistema..",
+        message: "O email informado não foi encontrado no sistema.",
         action: "Verifique se o email está digitado corretamente.",
       });
     }
@@ -192,6 +223,7 @@ async function hashPassordInObject(userInputValues) {
 
 const user = {
   create,
+  findOneById,
   findOneByUsername,
   update,
   findOneByEmail,
