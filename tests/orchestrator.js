0223f4706fb0367a22411df061ab0eb7d3efdfc0
Rafael Polmann <rafapolmann@hotmail.com>
2025-10-22 19:07:31 +0000

    feat: add `activation.findOneValidById()` and `orchestratod.extractUUID()`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -88,6 +88,11 @@ async function getLastEmail() {
   return lastEmailItem;
 }
 
+function extractUUI(text) {
+  const match = text.match(/[0-9a-fA-F-]{36}/);
+  return match ? match[0] : null;
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
@@ -96,6 +101,7 @@ const orchestrator = {
   createSession,
   deleteAllEmails,
   getLastEmail,
+  extractUUI,
 };
 
 export default orchestrator;
