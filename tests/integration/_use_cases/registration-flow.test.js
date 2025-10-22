aa51665967dea6c6ea7b0d41725796ab3e7917d9
Rafael Polmann <rafapolmann@hotmail.com>
2025-10-22 20:11:46 +0000

    feat: add `PATCH` `api/v1/activations/[token_id]`

--- tests/integration/_use_cases/registration-flow.test.js
+++ tests/integration/_use_cases/registration-flow.test.js
@@ -1,5 +1,6 @@
-import webserver from "infra/webserver";
-import activation from "models/activation";
+import webserver from "infra/webserver.js";
+import activation from "models/activation.js";
+import user from "models/user.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
@@ -11,6 +12,8 @@ beforeAll(async () => {
 
 describe("Use case: Resgitration Flow (all successful", () => {
   let createUserResponseBody;
+  let activationTokenId;
+
   test("Create user account", async () => {
     const createUserResponse = await fetch(
       "http://localhost:3000/api/v1/users",
@@ -50,7 +53,7 @@ describe("Use case: Resgitration Flow (all successful", () => {
     expect(lastEmail.subject).toBe("Ative seu cadastro no FinTab!");
     expect(lastEmail.text).toContain("RegistrationFlow");
 
-    const activationTokenId = orchestrator.extractUUI(lastEmail.text);
+    activationTokenId = orchestrator.extractUUI(lastEmail.text);
 
     expect(lastEmail.text).toContain(
       `${webserver.origin}/cadastro/ativar/${activationTokenId}`,
@@ -63,7 +66,23 @@ describe("Use case: Resgitration Flow (all successful", () => {
     expect(activationTokenObject.used_at).toBe(null);
   });
 
-  test("Active account", async () => {});
+  test("Active account", async () => {
+    const activationResponse = await fetch(
+      `http://localhost:3000/api/v1/activations/${activationTokenId}`,
+      {
+        method: "PATCH",
+      },
+    );
+
+    expect(activationResponse.status).toBe(200);
+
+    const activationResponseBody = await activationResponse.json();
+
+    expect(Date.parse(activationResponseBody.used_at)).not.toBeNaN();
+
+    const activatedUser = await user.findOneByUsername("RegistrationFlow");
+    expect(activatedUser.features).toEqual(["create:session"]);
+  });
 
   test("Login", async () => {});
 
