444d22e7ffa86c636c04e9d6ef8a88ad52f351f8
Rafael Polmann <rafapolmann@hotmail.com>
2026-02-02 19:57:41 +0000

    feat: apply `authorization.filterOutput()` tp endpoints

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -32,10 +32,17 @@ async function postHandler(request, response) {
   const newSession = await session.create(authenticatedUser.id);
   controller.setSessionCookie(newSession.token, response);
 
-  return response.status(201).json(newSession);
+  const secureOutputValues = authorization.filterOutput(
+    authenticatedUser,
+    "read:session",
+    newSession,
+  );
+
+  return response.status(201).json(secureOutputValues);
 }
 
 async function deleteHandler(request, response) {
+  const userTryingToDelete = request.context.user;
   const sessionToken = request.cookies.session_id;
 
   const sessionObject = await session.findOneValidByToken(sessionToken);
@@ -43,5 +50,11 @@ async function deleteHandler(request, response) {
 
   controller.clearSessionCookie(response);
 
-  return response.status(200).json(expiredSession);
+  const secureOutputValues = authorization.filterOutput(
+    userTryingToDelete,
+    "read:session",
+    expiredSession,
+  );
+
+  return response.status(200).json(secureOutputValues);
 }
